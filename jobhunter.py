70bc5671a1a8d04c8a8ab0745ac887501def88cd
ScottA22 <148298738+ScottA22@users.noreply.github.com>
2024-02-07 23:37:03 -0800

    Added connect to sql and create table

--- jobhunter.py
+++ jobhunter.py
@@ -8,6 +8,13 @@ import requests
 
 # https://remotive.com/api/remote-jobs
 
+def connect_to_sql():
+    connection = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='jobhunter_db')
+    return connection
+
+def create_table(cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS characters (id INT PRIMARY KEY auto_increment, title TEXT, company_name TEXT, category TEXT, job_type TEXT, candidate_required_location TEXT, salary INT, description TEXT)''')
 
 
-print('test')
+
+
